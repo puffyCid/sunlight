48e21ca583f679b08253db08a9c7c3d116a235f1
puffycid <puffycyan@gmail.com>
2025-12-21 19:22:36 -0500

    formatting

--- tests/proto_parser.rs
+++ tests/proto_parser.rs
@@ -1,5 +1,5 @@
-use std::{fs::read, path::PathBuf};
 use serde_json::json;
+use std::{fs::read, path::PathBuf};
 use sunlight::light::extract_protobuf;
 
 #[test]
@@ -11,6 +11,22 @@ fn test_protobuf_parser() {
     let value = extract_protobuf(&bytes).unwrap();
 
     assert_eq!(value.len(), 7);
-    assert_eq!(value.get(&23).unwrap().value.as_array().unwrap().len(), 1736);
-    assert_eq!(value.get(&23).unwrap().value.as_array().unwrap()[23].as_object().unwrap().get("10").unwrap().as_object().unwrap().get("value").unwrap().as_number().unwrap(), json!(4099380458 as u32).as_number().unwrap())
+    assert_eq!(
+        value.get(&23).unwrap().value.as_array().unwrap().len(),
+        1736
+    );
+    assert_eq!(
+        value.get(&23).unwrap().value.as_array().unwrap()[23]
+            .as_object()
+            .unwrap()
+            .get("10")
+            .unwrap()
+            .as_object()
+            .unwrap()
+            .get("value")
+            .unwrap()
+            .as_number()
+            .unwrap(),
+        json!(4099380458 as u32).as_number().unwrap()
+    )
 }
